b76b79b324dbba00a9c7aec835c402df4aa83ff4
Daniel Staudigel <dstaudigel@gmail.com>
2013-05-15 08:30:49 -0700

    You need to add an id='stage' element in order for the tests to run.

--- src/common.js
+++ src/common.js
@@ -2,6 +2,12 @@
 angular.module('$cordova.config', []).value('$cordova.config', {});
 angular.module('$cordova', ['$cordova.config']);
 
+(function create_stage() {
+   var el = document.createElement("div");
+   el.id = 'stage';
+   document.body.appendChild(el);
+})();
+
 window.app = {
     // Application Constructor
     initialize: function() {
@@ -14,7 +20,12 @@ window.app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
       console.log('bindEvents');
-      document.addEventListener('deviceready', this.onDeviceReady, false);
+      var _this = this;
+      window.document.addEventListener(
+	'deviceready', function() { 
+	  console.log("RECV deviceready");
+	  _this.onDeviceReady.apply(_this,arguments);
+	}, false);
     },
     // deviceready Event Handler
     //
